5da6d9a4cbfc82266593a0d81b325f2cee0305a8
marcelofcruz <marcelocdcruz@gmail.com>
2021-12-22 00:46:26 -0300

    inicio ao estudo de javascript

---
+++ sintaxe-basica-javascript/aula-2/js/script.js
@@ -0,0 +1,15 @@
+// Tipos Primitivos
+
+// boolean
+var vOuF = true
+console; log(typeof (vOuF))
+
+// number
+var idade = 24
+console.log(typeof (idade))
+
+// string
+var nome = 'Marcelo'
+console.log(typeof (nome))
+
+// 
---
+++ sintaxe-basica-javascript/aula-3/js/script.js
@@ -0,0 +1,8 @@
+// Array
+let arrayNome = ['marcelo', 'angel', 'benjamin']
+// console.log(arrayNome)
+
+// foreach
+arrayNome.forEach(function (item, index) { console.log(index + 1, item) })
+
+// AULA 4 - VETORES E ARRAYS PAUSADO: 6:32
